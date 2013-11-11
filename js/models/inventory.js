f3d23839b5acdc7f738ccf4c2f3c1855a9f53701
zoie0312 <greenmoonshisu@gmail.com>
2013-11-11 20:58:44 +0000

    modify recipe and inventory definition

--- js/models/inventory.js
+++ js/models/inventory.js
@@ -1,8 +1,8 @@
 // this model represents what raw materials a user already has at home
 // for more info. about this project, please check README.md
-var APP.Inventory = Backbone.Models.extend({
+APP.Inventory = Backbone.Models.extend({
 	defaults: {
-		user: default_user,
+		//user: default_user,
 		materials: ["onion", "garlic", "chicken", "tomato"]
 	}
 
--- js/models/recipe.js
+++ js/models/recipe.js
@@ -1,6 +1,6 @@
 // this model represents each recipe in our database
 // for more info. about this project please check README.md
-var APP.Recipe = Backbone.Model.extend({
+APP.Recipe = Backbone.Model.extend({
 	defaults: {
 		name: "Three Cup Chicken",
 		ingredients: ["chicken", "ginger", "garlic"]
