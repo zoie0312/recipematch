ff46a028085f15dd244d15ac8578655fad7755e9
zoie0312 <greenmoonshisu@gmail.com>
2013-11-20 05:01:13 +0000

    modify ingredient model view

--- js/views/ingredient_view.js
+++ js/views/ingredient_view.js
@@ -1,23 +1,34 @@
 APP.IngredientView = Backbone.View.extend({
 	tagName: "div",
 	className: "ingredient",
+
 	template: _.template('<label><%= name %></label>' +
 		'<button><span class="glyphicon glyphicon-remove"></span></button>'),
+	ing_template: _.template($('#ingredient-template').html()),
 
 	events: {
-		"click .glyphicon-remove": "remove"
+		"click .destroy": "clear"
+
+	},
+
+	initialize: function(){
+		this.listenTo(this.model, 'destroy', this.remove);
 
 	},
 
 	render: function(){
 		//this.$el.html('<h3>' + this.model.get('name') + '</h3>');
 		var attributes = this.model.toJSON();
-		this.$el.html(this.template(attributes));
+		this.$el.html(this.ing_template(attributes));
+		this.$input = this.$('.edit');
 		return this;
 	},
 
-	remove: function(){
-		this.$el.remove();
+	clear: function(){
+		//console.log("IngredientView: remove() called");
+		//console.log("ingredient name= " + this.model.get('name'));
+		//this.$el.remove();
+		this.model.destroy();
 
 	}
 });
